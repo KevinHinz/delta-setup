9f6988aec1edf6debfc3046c50be3373fa1fdde5
KevinHinz <kevin.hinz232@gmail.com>
2022-12-28 19:31:14 +0100

    set home dir to -1 & shift ramps conn.

--- CeramicPrinter_MarlinLCD_MaPS_20160429/Configuration.h
+++ CeramicPrinter_MarlinLCD_MaPS_20160429/Configuration.h
@@ -304,9 +304,9 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
-#define X_HOME_DIR 1
-#define Y_HOME_DIR 1
-#define Z_HOME_DIR 1
+#define X_HOME_DIR -1
+#define Y_HOME_DIR -1
+#define Z_HOME_DIR -1
 
 #define min_software_endstops true //If true, axis won't move to coordinates less than *_MIN_POS.
 #define max_software_endstops true //If true, axis won't move to coordinates greater than *_MAX_POS.
